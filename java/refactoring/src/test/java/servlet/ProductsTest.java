fa0727dbcb66502810856fba87465bf86685e3e0
valentinnodan <valentinka-danilova@mail.ru>
2020-10-24 23:10:00 +0300

    initial tests

---
+++ java/refactoring/src/test/java/servlet/ProductsTest.java
@@ -0,0 +1,20 @@
+package servlet;
+
+import org.junit.Test;
+
+public class ProductsTest {
+    @Test
+    public void addProductsTest(){
+
+    }
+
+    @Test
+    public void getProductsTest(){
+
+    }
+
+    @Test
+    public void queryTest(){
+
+    }
+}

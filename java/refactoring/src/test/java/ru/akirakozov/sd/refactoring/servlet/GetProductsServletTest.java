dfb1342500e4595457bde5e1e66595f649827df1
valentinnodan <valentinka-danilova@mail.ru>
2020-10-25 12:11:13 +0300

    added tests for get

--- java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServletTest.java
+++ java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServletTest.java
@@ -1,7 +1,117 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class GetProductsServletTest {
+    GetProductsServlet getServlet;
+    @Mock
+    HttpServletRequest myRequest;
+    @Mock
+    HttpServletResponse myResponse;
+    Writer myWriter;
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        getServlet = new GetProductsServlet();
+        myWriter = new StringWriter();
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+            Statement stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+
+            String name = "potato";
+            int price = 5;
+            sql = "INSERT INTO PRODUCT " +
+                    "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
+            stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+
+            name = "tomato";
+            price = 10;
+            sql = "INSERT INTO PRODUCT " +
+                    "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
+            stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+
+            name = "onion";
+            price = 15;
+            sql = "INSERT INTO PRODUCT " +
+                    "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
+            stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @After
+    public void after() {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "DROP TABLE PRODUCT";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private List<ProductItem> getProducts() {
+        List<ProductItem> products = new ArrayList<>();
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                products.add(new ProductItem(name, price));
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return products;
+    }
+
+    @Test
+    public void getTest() throws IOException {
+        when(myResponse.getWriter()).thenReturn(new PrintWriter(myWriter));
+        List<ProductItem> products = getProducts();
+
+        getServlet.doGet(myRequest, myResponse);
+
+        List<ProductItem> productsAfter = getProducts();
+        assertEquals("<html><body>\n" +
+                "potato\t5</br>\n" +
+                "tomato\t10</br>\n" +
+                "onion\t15</br>\n" +
+                "</body></html>\n", myWriter.toString());
+        assertEquals(products.size(), productsAfter.size());
+    }
 
 }
